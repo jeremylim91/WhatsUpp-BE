c04da168318a7b1724ea93641ad1329749846c7f
jeremylim91 <jeremylim_91@live.com>
2021-05-30 22:55:12 +0800

    trimmed unwanted code

--- config/dbconfig.mjs
+++ config/dbconfig.mjs
@@ -1,4 +1,3 @@
-import mongoose from 'mongoose';
 export const config = {
   MONGO_USERNAME: 'jeremylim',
   MONGO_PASSWORD: null,
@@ -8,12 +7,3 @@ export const config = {
 };
 
 export const connectionUri = `mongodb://${config.MONGO_USERNAME}:${config.MONGO_PASSWORD}@${config.MONGO_HOSTNAME}:${config.MONGO_PORT}/${config.MONGO_DB}?authSource=admin`;
-
-// const MONGO_HOSTNAME = '127.0.0.1';
-// const MONGO_PORT = '27017';
-// const MONGO_DB = 'whatsuppDB';
-// const url = `mongodb://${MONGO_HOSTNAME}:${MONGO_PORT}/${MONGO_DB}`;
-
-// const mongoose = require('mongoose');
-// const connectToDb = () => mongoose.connect(url, {useNewUrlParser: true});
-// export default connectToDb;
