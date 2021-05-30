a7b573fa322981814df485e85ca4e07f7b001bf4
jeremylim91 <jeremylim_91@live.com>
2021-05-30 14:46:17 +0800

    created express app, middleware, and connection to mongodb server

---
+++ index.mjs
@@ -0,0 +1,47 @@
+import express from 'express';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import methodOverride from 'method-override';
+import bindRoutes from './routes.mjs';
+import mongoose from 'mongoose';
+// import {connectionUri, config} from './config/dbconfig.mjs';
+// Initialise Express instance
+const app = express();
+
+// Set CORS headers
+app.use(
+  cors({
+    credentials: true,
+    origin: true,
+  })
+);
+// Set the Express view engine to expect EJS templates
+app.set('view engine', 'ejs');
+// Bind cookie parser middleware to parse cookies in requests
+app.use(cookieParser());
+// Bind Express middleware to parse request bodies for POST requests
+app.use(express.urlencoded({extended: false}));
+// Bind Express middleware to parse JSON request bodies
+app.use(express.json());
+// Bind method override middleware to parse PUT and DELETE requests sent as POST requests
+app.use(methodOverride('_method'));
+// Expose the files stored in the public folder
+app.use(express.static('public'));
+
+// Bind route definitions to the Express application
+bindRoutes(app);
+
+const mongoURI = 'mongodb://localhost:27017/whatsuppDB';
+
+mongoose
+  .connect(mongoURI, {useNewUrlParser: true, useUnifiedTopology: true})
+  .then(() => {
+    console.log('connected to db');
+
+    // Set Express to listen on the given port
+    const PORT = process.env.PORT || 3004;
+    app.listen(PORT, () => console.log(`listening on port ${PORT}`));
+  })
+  .catch((error) => {
+    console.log(error);
+  });
