0868aaa4bf0ca38295c6fb4dc568789ab2945501
jeremylim91 <jeremylim_91@live.com>
2021-06-01 13:37:55 +0800

    created the shell of the controller.

--- controllers/users.mjs
+++ controllers/users.mjs
@@ -1,1 +1,10 @@
-export default function initUsersController(db) {}
+export default function initUsersController(db) {
+  // const allRoomsByUser = async (req, res) => {
+  //   const {userId} = req.params;
+  //   // create a find query which searches the db for rooms where the username is
+  //   const allRooms = db.Room.find({username: userId});
+  // };
+  // return {
+  //   allRoomsByUser,
+  // };
+}
