25262c38da2022467486a5546f1ff2e4c012df78
jeremylim91 <jeremylim_91@live.com>
2021-06-01 13:39:41 +0800

    added query to find all rooms in collection

--- controllers/rooms.mjs
+++ controllers/rooms.mjs
@@ -1,1 +1,17 @@
-export default function initRoomsController(db) {}
+export default function initRoomsController(db) {
+  const index = async (req, res) => {
+    // query the db to get all the rooms
+    try {
+      const allRooms = await db.Room.find();
+      console.log(allRooms);
+      res.send(allRooms);
+    } catch (error) {
+      console.log(error);
+      // pass errors to express to use its default error handler
+      next(error);
+    }
+  };
+  return {
+    index,
+  };
+}
