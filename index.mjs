e3aa8ecef1e42993fada983276704339dc201a2a
jeremylim91 <jeremylim_91@live.com>
2021-06-14 11:58:39 +0800

    reconfigured socket io and express

--- index.mjs
+++ index.mjs
@@ -11,14 +11,15 @@ const PORT = process.env.PORT || 3004;
 
 // Initialise an Express instance; note: do not start listening onto the port until the db is connected. This is to ensure that the server can't receive requests until the db is ready
 const app = express();
-const httpServer = http.createServer(app);
+// const httpServer = http.createServer(app);
+// WORKING:
 // create a new socket.io instance and attach it to the http server.
-const io = new Server(httpServer, {
-  cors: {
-    credentials: true,
-    origin: true,
-  },
-});
+// const io = new Server(httpServer, {
+//   cors: {
+//     credentials: true,
+//     origin: true,
+//   },
+// });
 
 // Set CORS headers
 app.use(
@@ -27,6 +28,7 @@ app.use(
     origin: true,
   })
 );
+
 // Set the Express view engine to expect EJS templates
 app.set('view engine', 'ejs');
 // Bind cookie parser middleware to parse cookies in requests
@@ -40,24 +42,45 @@ app.use(methodOverride('_method'));
 // Expose the files stored in the public folder
 app.use(express.static('public'));
 
-// Bind route definitions to the Express application
-bindRoutes(app);
-
 // connect the db
 try {
+  // the "await" above has a blocking effect to ensure tt we don't start listening on the port before the db is up (db connection is a async process)
   await mongoose.connect(mongoURI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   });
   console.log('db connected successfully');
+  // working:
+  // httpServer.listen(PORT, () => console.log(`listening on ${PORT}`));
 
-  // the "await" above has a blocking effect to ensure tt we don't start listening on the port before the db is up (db connection is a async process)
-  httpServer.listen(PORT, () => console.log(`listening on ${PORT}`));
+  // also working (better version):
+  const server = app.listen(PORT, () =>
+    console.log(`listening on port ${PORT}`)
+  );
+
+  const io = new Server(server, {
+    cors: {
+      credentials: true,
+      origin: true,
+    },
+  });
+
+  // Bind route definitions to the Express application
+  bindRoutes(app, io);
+
+  // io.on('connection', (socket) => {
+  //   console.log('Client connected');
+  // });
 } catch (error) {
   console.log(error);
 }
-
-io.on('connection', (socket) => {
-  console.log(`socket.id is:`);
-  console.log(socket.id);
-});
+// io.on('connection', (socket) => {
+//   console.log(`socket.id is:`);
+//   console.log(socket.id);
+//   // handle emit message 'addMsgToDb'
+//   socket.on('addMsgToDb', (msg) => {
+//     // save the msg to the db
+//     // broadcast msg back to client
+//     io.emit('receiveMsg', msg);
+//   });
+// });
